abff5b1b1405f08751ece9a911e4ba30fb9586a8
Kashyap2424 <kapdikashyap2003@gmail.com>
2024-04-15 14:48:26 +0530

    Solve navigation error in project.

--- App.tsx
+++ App.tsx
@@ -4,32 +4,30 @@
  *
  * @format
 */
-import 'react-native-gesture-handler'
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 
-import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { Drawer } from 'react-native-drawer-layout';
 
 import Home from './Screens/Home';
 import Settings from './Screens/Settings';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
+const Drawer = createDrawerNavigator()
 
 function App(): React.JSX.Element {
-  return (
-    <SafeAreaView>
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Home} />
-          <Drawer.Screen name="Settings" component={Settings} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+  const [open, setOpen] = React.useState(false);
 
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
